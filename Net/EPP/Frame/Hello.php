a7cd69890cfead4f43e3e468484d7b21ada18e5e
Gavin <gavin.brown@centralnic.com>
2015-08-04 18:46:21 +0100

    tidy

--- Net/EPP/Frame/Hello.php
+++ Net/EPP/Frame/Hello.php
@@ -1,11 +1,11 @@
 <?php
 
-	/**
-	* @package Net_EPP
-	*/
-	final class Net_EPP_Frame_Hello extends Net_EPP_Frame {
-		function __construct() {
-			parent::__construct('hello');
-		}
+/**
+* @package Net_EPP
+*/
+final class Net_EPP_Frame_Hello extends Net_EPP_Frame {
+	function __construct() {
+		parent::__construct('hello');
+		$this->command = 'hello';
 	}
-?>
+}
